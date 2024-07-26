abf7374e2c16772cc41ab8f73742d90e53102f80
TwoRulonOboev <145360964+TwoRulonOboev@users.noreply.github.com>
2024-07-26 15:23:25 +0200

    Вывод Мудрого дуба на игровую площадку

--- GraphQL/Data/Query.cs
+++ GraphQL/Data/Query.cs
@@ -7,11 +7,13 @@ namespace GraphQL.Data
     {
         private readonly IRepository<Album> _albumRepository;
         private readonly IRepository<Photo> _photoRepository;
+        private readonly ITreeManager _treeManager;
 
-        public Query(IRepository<Album> albumRepository, IRepository<Photo> photoRepository)
+        public Query(IRepository<Album> albumRepository, IRepository<Photo> photoRepository, ITreeManager treeManager)
         {
             _albumRepository = albumRepository;
             _photoRepository = photoRepository;
+            _treeManager = treeManager;
         }
 
         public async Task<List<Album>> GetAlbumsAsync() => await _albumRepository.GetAsync();
@@ -21,5 +23,7 @@ namespace GraphQL.Data
         public async Task<List<Photo>> GetPhotos() => await _photoRepository.GetAsync();
 
         public async Task<Photo> GetPhoto(int id) => await _photoRepository.GetAsync(id);
+
+        public Node GetTreeRoot(int treeId) => _treeManager.GetRootOfTree(treeId);
     }
 }
